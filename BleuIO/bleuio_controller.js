45f253ad38967407c11f80c82b4e8e1773bd8339
AntonDmitriev <44076196+AntonDmitriev@users.noreply.github.com>
2023-09-25 01:47:07 -0500

    Abstracted enough for writing trilateration code >:)

--- BleuIO/bleuio_controller.js
+++ BleuIO/bleuio_controller.js
@@ -2,46 +2,24 @@ import init_bleuIO from './bleuio_utils.js'
 
 export default async function CreateScanner(bleuIO_device_path, interval, func) {
 
-  let MAP_ADDR_TO_RSSI = {};
-
   const bleuIO = init_bleuIO(bleuIO_device_path);
 
   // Each time a gap scan result is printed on the terminal it will update our table
   function onNewLine(scan) {
-    //MAP_ADDR_TO_RSSI[scan.addr] = scan.rssi;
     func(scan);
-    // console.log('s'+scan);
-    // console.log(MAP_ADDR_TO_RSSI); 
-
+    // console.log('scan'+scan);
   }
 
-  // I only think GAPSCAN ever recognizes new devices
-  // it will never re-find old ones
+  bleuIO.onReadableEvent(onNewLine); // Binds a function to each new line BleuIO outputs onto serial
 
-  // So, I think we need to do an AT Target Scan
-  // to do this we make a map of the the non-distractor tags
-  // figure out what their MAC address is
-  // and only target scan these addresses.
-  // = note: you can only target scan 3 things at a time.
-  // Problem: This doesn't really let us detect distractors
-
-  // Surely there must be some way to just re-scan everything at a specific interval
-  // Figure out how to ssh into the dongle
-  // try issuing commands from there.
-
-  bleuIO.onReadableEvent(onNewLine); // Prints out everything BleuIO outputs onto our terminal
-
-  //await bleuIO.openPort();
-  await bleuIO.writeData('ATV1'); // Turned verbose mode on/off
+  await bleuIO.writeData('ATV1'); // Turned verbose mode on 
   await bleuIO.setCentralRole(); // This isnt working - the port.on listener is also not running.
 
   setInterval( async () => {
-    await bleuIO.gapScan(interval); // Run gap scans at 3 sec interval
+    await bleuIO.gapScan(interval); // Run gap scans at an interval
   }, interval*1000);
 
   // Once we have initialized the BleuIO device to work as a scanner
   // return a function that lets you read MAP_ADDR_TO_RSSI
-  return () => {
-            return MAP_ADDR_TO_RSSI;
-          }
+
 }
--- Localization/index.js
+++ Localization/index.js
@@ -9,21 +9,18 @@ let AddressToRssi_ByDevice = {};
 DevicePaths.forEach( 
     async (name) => { 
 
-        AddressToRssi_ByDevice[name] = {}; // Register the device with an ADDR to RSSI map
+        // Associate the device with an ADDR to RSSI map
+        AddressToRssi_ByDevice[name] = {};
 
-        // Initialize a BleuIO device
+        // Initialize the BleuIO device
         await CreateScanner(name, SCAN_INTERVAL, 
 
             // Pass in a function to run on every gapscan
             (update) => {
-                
                     // Each new line spit out by the gap scan gives us an update
-                    // to our map.
-
-                    // Apply update to the map that corresponds to our device
-
+                    // to our map. Apply update to the map that corresponds to our device.
                     AddressToRssi_ByDevice[name][update.addr] = update.rssi
-                    // Run trilateration again
+                    // Re-run trilateration
                     Trilaterate();
             }
 
@@ -32,9 +29,11 @@ DevicePaths.forEach(
 
 function Trilaterate() {
 
-    console.log(AddressToRssi_ByDevice);
     // Assume all of AddressToRssi_ByDevice is always up to date
     // Given this information, you should be able to:
     // Update AddressToDistance map by trilateration
+
+    console.log(AddressToRssi_ByDevice);
+
 
 }
