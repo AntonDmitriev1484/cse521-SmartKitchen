30a472029f938d1c56e25049b6cf2dc4bbdd0284
breadnmkii <b.z.yang@wustl.edu>
2023-11-08 17:14:19 -0600

    Python is not reading anything off of serial I am confusion

--- Python_App/app.py
+++ Python_App/app.py
@@ -1,6 +1,10 @@
 from smartkitchen import controller
 from smartkitchen import util
 
+# Command to see all devices:
+# 
+
+
 # List devices
 print("\n=== Finding devices connected to serial port...")
 devices =  util.DiscoverSerialDevices()
@@ -10,4 +14,15 @@ for devc in devices:
 
 # Create BleuIO instance
 print("\n=== Attempting to create a BleuIO receiver instance...")
-controller.testCreateDevice()
+Scanner = controller.Scanner('/dev/ttyACM0')
+# Scanner.scan()
+
+# controller.gapscanReceiver(bleuIO)
+
+# # bleuIO.at_central()
+
+# # def scan_cb(scan_input):
+# #     print(scan_input)
+
+# # bleuIO.register_scan_cb(scan_cb)
+
--- Python_App/smartkitchen/controller.py
+++ Python_App/smartkitchen/controller.py
@@ -1,41 +1,65 @@
 from bleuio_lib.bleuio_funcs import BleuIO
 from bleuio_lib.exceptions import *
+import time
+import serial
 
 ''' PyPip Docs: https://pypi.org/project/bleuio/#description
     Check README and requirements.txt for necessary dependencies
 '''
 
-def testCreateDevice():
-    try:
-        bleReceiver = BleuIO()
-    except (OSError, BleuIoError, IndexError) as e:
-        print("Error: Could not instantiate BleuIO receiver device")
+class Scanner:
 
-        if (type(e) == IndexError):
-            print(" | Could not autodetect any connected BleuIO devices")
+    def __init__(self, port):
+       
+        self.ser = serial.Serial(
+            port=port,
+            baudrate=115200,
+            bytesize=serial.EIGHTBITS,
+            parity=serial.PARITY_NONE,
+            stopbits=serial.STOPBITS_ONE,
+            timeout=1  # Adjust timeout as needed
+        )
 
-        return
+        self.ser_write('ATI')
+        time.sleep(2)
 
-    print("Successfully created BleuIO device")
-
-    # ATI info
-    atiResponse = bleReceiver.ati()
-    print(atiResponse.Cmd)
-    print(atiResponse.Ack)
-    print(atiResponse.Rsp)
-    print(atiResponse.End)
+        # line = self.ser.read(1000).decode('utf-8').strip()
+        # print(line)
+        self.ser_read(2) # Ok it genuinely seems like nothing is being written to serial
+        
+        # self.ser_write('AT+CENTRAL')
+        # lines = self.ser.readlines()
 
 
-def gapscanReceiver(receiver: BleuIO):
-    # Put receiver into dual role mode
-    receiver.at_dual()
+    def scan(self):
+        while True:
+            time.sleep(2)
+            print("Scanning")
+            self.ser_write('AT+GAPSCAN\n')
+            self.ser_read(1)
+            
 
-    # Perform scan
-    res = receiver.at_gapscan()
-
-    # Return results (is the scan blocking?)
-    print(res.Cmd)
-    print(res.Ack)
-    print(res.Rsp)
-    print(res.End)
+    # Write a string to serial
+    def ser_write(self, command):
+        # Ensure the command is encoded as bytes before sending
+        command_bytes = command.encode('utf-8')
+        self.ser.write(command_bytes)
+    
+    # Read strings off of serial for 'timer' number of seconds
+    def ser_read(self, timer):
+        while True:
+            line = self.ser.readline().decode('utf-8').strip()
+            print(line)
+        # # Get the start time
+        # start_time = time.time()
+        # # Loop for the specified duration
+        # while time.time() - start_time < timer:
+        #     print("in")
+        #     # Read line of text from serial port
+            # lines = self.ser.readlines()
+            # # Print the received text
+            # for line in lines:
+            #     print("Received:" + line.decode('utf-8').strip())
+            
+            
 
