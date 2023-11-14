c00d9dd0ccf72156feab6a1dca59109abe2cf379
Jason Li <j.m.li@wustl.edu>
2023-11-13 23:52:19 -0600

    changed

--- Python_App/app.py
+++ Python_App/app.py
@@ -8,8 +8,20 @@ import time
 # Maps ip -> (Name, T=valid item / F=distractor)
 IP_TO_NAME = {
     "[0]C3:00:00:0B:1A:7C": ("Oatmeal", True),
-    "[0]C3:00:00:0B:1A:7A": ("Communist Manifesto", True)
+    "[0]C3:00:00:0B:1A:7A": ("Communist Manifesto", True),
+    "Placeholder" : ("Salt", True),
+    "Placeholder" : ("1 Measure Cup", True),
+    "Placeholder" : ("½ Measure Cup", True),
+    "Placeholder" : ("¼ Measure Spoon", True),
+    "Placeholder" : ("Pan", True),
+    "Placeholder" : ("Stirring Spoon", True),
+    "Placeholder" : ("Timer", True),
+    "Placeholder" : ("Bowl", True),
+    "Placeholder" : ("Metal Spoon", True),
+    "Placeholder" : ("Cork Hot Pad", True),
 }
+
+itemsOnTable = []
 
 
 def main():
@@ -41,8 +53,94 @@ def main():
         print("Main")
         
         # !!! INSERTION TRILATERATION LOGIC HERE OR IN BEACONINFO !!!
-        
+
         trilateration_table.print()
 
 main()
+
+
+import pyttsx3
+import time
+
+
+
+def requires(inputArray):
+    engine = pyttsx3.init()  # object creation
+    """ RATE"""
+    rate = engine.getProperty('rate')  # getting details of current speaking rate
+    print(rate)  # printing current voice rate
+    engine.setProperty('rate', 100)  # setting up new voice rate
+
+    requireString = "The following items are required."
+
+    """VOLUME"""
+    volume = engine.getProperty('volume')  # getting to know current volume level (min=0 and max=1)
+    print(volume)  # printing current volume level
+    engine.setProperty('volume', 2.0)  # setting up volume level  between 0 and 1
+
+    """VOICE"""
+    voices = engine.getProperty('voices')  # getting details of current voice
+    # engine.setProperty('voice', voices[0].id)  #changing index, changes voices. o for male
+
+    engine.say("Put the following items on the table.")
+    engine.runAndWait()
+    for item in inputArray:
+        engine.say(item)
+        engine.runAndWait()
+    engine.stop()
+
+inputTest = ['Spoon', 'Oatmeal', 'Pot', 'Water']
+
+# requires(inputTest)
+
+def distractorPresent():
+    engine = pyttsx3.init()  # object creation
+    """ RATE"""
+    rate = engine.getProperty('rate')  # getting details of current speaking rate
+    print(rate)  # printing current voice rate
+    engine.setProperty('rate', 100)  # setting up new voice rate
+
+    requireString = "The following items are required."
+
+    """VOLUME"""
+    volume = engine.getProperty('volume')  # getting to know current volume level (min=0 and max=1)
+    print(volume)  # printing current volume level
+    engine.setProperty('volume', 2.0)  # setting up volume level  between 0 and 1
+
+    """VOICE"""
+    voices = engine.getProperty('voices')  # getting details of current voice
+    # engine.setProperty('voice', voices[0].id)  #changing index, changes voices. o for male
+
+    engine.say("There is a distractor on the table. Please remove it.")
+    engine.runAndWait()
+    engine.stop()
+
+def correctItemAdded(ItemName):
+    engine = pyttsx3.init()  # object creation
+    """ RATE"""
+    rate = engine.getProperty('rate')  # getting details of current speaking rate
+    print(rate)  # printing current voice rate
+    engine.setProperty('rate', 100)  # setting up new voice rate
+
+    requireString = "The following items are required."
+
+    """VOLUME"""
+    volume = engine.getProperty('volume')  # getting to know current volume level (min=0 and max=1)
+    print(volume)  # printing current volume level
+    engine.setProperty('volume', 2.0)  # setting up volume level  between 0 and 1
+
+    """VOICE"""
+    voices = engine.getProperty('voices')  # getting details of current voice
+    engine.setProperty('voice', voices[0].id)  #changing index, changes voices. o for male
+    outputText = "The "
+    outputText = outputText + ItemName
+    outputText = outputText + " has been detected on the table. Good job! Smiling Emoji"
+    engine.say(outputText)
+    engine.runAndWait()
+    engine.stop()
+
+correctItemAdded("Brendan Yang")
+
+
+requires(requiredItems)
 
