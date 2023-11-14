ccd71c6b0bb82a3915caa201944dd823a93a1e90
Jason Li <j.m.li@wustl.edu>
2023-11-13 21:14:14 -0600

    speech

---
+++ VoiceScript.py
@@ -0,0 +1,26 @@
+import pyttsx3
+import time
+engine = pyttsx3.init() # object creation
+
+""" RATE"""
+rate = engine.getProperty('rate')   # getting details of current speaking rate
+print (rate)                        #printing current voice rate
+engine.setProperty('rate', 75)     # setting up new voice rate
+
+
+"""VOLUME"""
+volume = engine.getProperty('volume')   #getting to know current volume level (min=0 and max=1)
+print (volume)                          #printing current volume level
+engine.setProperty('volume',2.0)    # setting up volume level  between 0 and 1
+
+"""VOICE"""
+voices = engine.getProperty('voices')       #getting details of current voice
+#engine.setProperty('voice', voices[0].id)  #changing index, changes voices. o for male
+engine.setProperty('voice', voices[1].id)   #changing index, changes voices. 1 for female
+
+engine.say("Put the spoon on the table.")
+engine.runAndWait()
+time.sleep(3)
+engine.say("Remove the distraction from the table.")
+engine.runAndWait()
+engine.stop()
