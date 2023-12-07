ea5d1312df1e742a10306a5dc9f6b10a02a1f5cf
breadnmkii <b.z.yang@wustl.edu>
2023-12-06 19:29:01 -0600

    Final demo version

--- NodeJS_App/BleuIO/index.js
+++ NodeJS_App/BleuIO/index.js
@@ -1,5 +1,7 @@
 import CreateScanner from './bleuio_controller.js'
 import { SerialPort } from "serialport";
+import * as child from 'child_process';
+import XRegExp from "xregexp";
 
 // import { express } from express;
 
@@ -8,7 +10,7 @@ import { SerialPort } from "serialport";
 
 const DevicePaths = ['/dev/ttyACM0', '/dev/ttyACM1', '/dev/ttyACM2','/dev/ttyACM3'];
 const SCAN_INTERVAL = 1;
-const SUBSCRIBER_URL = 'http://172.27.177.163:3000';
+const SUBSCRIBER_URL = 'http://172.27.177.63:3000';
 
 // AT+DIS to find the serial number of the BleuIO running on each port
 // You can run ./findDev.sh on the pi to figure out the serial code of each pi
@@ -18,39 +20,59 @@ const SUBSCRIBER_URL = 'http://172.27.177.163:3000';
 // Maps ip -> (Name, T=valid item / F=distractor)
 const IP_TO_NAME = {
   "[0]C3:00:00:0B:1A:7C": ("Oatmeal", true),
-  "[0]C3:00:00:0B:1A:7A": ("Distractor", false),
+  "[0]C3:00:00:0B:1A:79": ("Distractor", false),
   "[0]C3:00:00:0B:1A:7B" : ("Salt", true),
-  "Placeholder" : ("1 Measure Cup", true),
+//   "Placeholder" : ("1 Measure Cup", true),
   "[0]C3:00:00:0B:1A:86" : ("½ Measure Cup", true),
   "[0]C3:00:00:0B:1A:8A" : ("¼ Measure Spoon", true),
-  "[0]C3:00:00:0B:1A:88" : ("Pan", true),
-  "Placeholder" : ("Stirring Spoon", true),
-  "[0]C3:00:00:0B:1A:87" : ("Timer", true),
+  "[0]C3:00:00:0B:1A:88" : ("Calibrator", true),
+//   "Placeholder" : ("Stirring Spoon", true),
+  "[0]C3:00:00:0B:1A:87" : ("DistractorV2", true), // Originally timer
   "[0]C3:00:00:0B:1A:89" : ("Bowl", true),
-  "Placeholder" : ("Metal Spoon", true),
-  "[0]C3:00:00:0B:1A:79" : ("Cork Hot Pad", true),
+//   "Placeholder" : ("Metal Spoon", true),
+//   "[0]C3:00:00:0B:1A:XX" : ("Cork Hot Pad", true),
 }
 
-// app.get('/get-calibration-point', (req, res) => {
-// })
+const SERID_TO_SENSID = {
+  "4048FDE85405": 0, // Top right
+  "4048FDE85428": 1, // Bottom right
+  "4048FDE853C6": 2, // Bottom left
+  "4048FDE85A27": 3, // Top left
+}
 
-// Exec is the kind of asynchronous that doesn't let you await
 
-// await exec('~/findDev.sh', (error, stdout, stderr) => {
-//     if (error) {
-//       console.error(`Error executing script: ${error.message}`);
-//       return;
-//     }
+child.exec('~/findDev.sh', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Error: ${error.message}`);
+    return;
+  }
 
-//     console.log('Script output:', stdout);
+  // Initialization code to map DevSerial paths /dev/ttyACM0 to physical sensor 
+  // ids by getting their serial numbers with a bash script
+  console.log('Command output:', stdout);
+  let lines = stdout.split('\n');
 
-//     if (stderr) {
-//       console.error('Script errors:', stderr);
-//     }
-//   }
-//   );
+  let DEVPATH_TO_SENSID = {};
 
-//app.get('/run', (req, res) => {
+  // Match: /dev/ttyACM2 - Smart_Sensor_Devices_BleuIO_4048FDE853C6
+  const reg = new RegExp(/^(.*) - (.*)$/);
+
+  console.log(lines);
+  lines.pop()
+
+  lines.forEach( (line) => {
+
+    let [full, devpath, serid] = line.match(reg);
+    serid = serid.match(/.{12}$/);
+    // last 12 char
+
+    console.log(devpath +" | "+serid);
+
+    DEVPATH_TO_SENSID[devpath] = SERID_TO_SENSID[serid]
+  })
+
+  console.log(DEVPATH_TO_SENSID);
+
   DevicePaths.forEach(
     async (port) => { // CHECK: port is the port of receiver?
         console.log(`Initializing ${port}`);
@@ -61,14 +83,23 @@ const IP_TO_NAME = {
             // Pass in a function, POSTs each ser_line to a Python server
             (ser_line) => {
              	// PATHLOSS DEBUG: hardcode check for our BLE calibration tag ([0]C3:00:00:0B:1A:88) 
-                //if (ser_line.addr == "[0]C3:00:00:0B:1A:7A") {
-                if (IP_TO_NAME[ser_line.addr]) {
+                //if (ser_line.addr == "[0]C3:00:00:0B:1A:79") {
+
+                // Sometimes it gets 'undefined' as its ser_line
+                //console.log(" Ser_line addr is: "+ser_line.addr+" map access gives: "+IP_TO_NAME[ser_line.addr])
+                // console.log(IP_TO_NAME[ser_line.addr][0] +" "+ IP_TO_NAME[ser_line.addr][0])
+                if (IP_TO_NAME[ser_line.addr] !== undefined) {
+              
                   const update = {
-                    'device':port,
+                    'device':DEVPATH_TO_SENSID[port],
                     'addr':ser_line.addr,
                     'rssi':ser_line.rssi
                   }
-                    // If this is one of our BLE beacons
+
+                  // if ( ser_line.addr === "[0]C3:00:00:0B:1A:79") {
+                  //   console.log("SENDING DISTRACTOR INFO");
+                  // }
+                  // If this is one of our BLE beacons
                     console.log(`Sending ser_line from ${port}`);
                     console.log(ser_line);
 
@@ -98,8 +129,6 @@ const IP_TO_NAME = {
         )
     }
     );
-
-
-//})
+  });
 
 
